a845e9cda52986f106427944f7801109d1cc6364
siriusblack034 <chien08082000@gmail.com>
2021-10-01 17:13:03 +0700

    deploy then update project

---
+++ src/constants/project/challenges/common/weather-forcase.js
@@ -0,0 +1,19 @@
+export default {
+  key: 'forecase-weather',
+  title: "Weather Forecast",
+  des: "<p>A website that helps you forecast the weather now and in the future. </p>",
+  headIcon: 'chat',
+  links: [
+    {
+      link: "https://github.com/siriusblack034/forecast-weather",
+      icon: 'github',
+      tooltip: 'Open In Github'
+    },
+  ],
+  techs: [
+    {
+      name: "Vue",
+      icon: 'vue'
+    }
+  ]
+}
